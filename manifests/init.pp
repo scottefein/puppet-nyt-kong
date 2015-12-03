6d2903835ff0184b04bd42e4c6d38ed6d8226eda
Scott Feinberg <scott.feinberg@nytimes.com>
2015-12-03 16:29:41 -0500

    some more refactoring, and reload

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,47 +1,70 @@
-# Class: kong
-# ===========================
-#
-# Full description of class kong here.
-#
-# Parameters
-# ----------
-#
-# Document parameters here.
-#
-# * `sample parameter`
-# Explanation of what this parameter affects and what it defaults to.
-# e.g. "Specify one or more upstream ntp servers as an array."
-#
-# Variables
-# ----------
-#
-# Here you should define a list of variables that this module would require.
-#
-# * `sample variable`
-#  Explanation of how this variable affects the function of this class and if
-#  it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#  External Node Classifier as a comma separated list of hostnames." (Note,
-#  global variables should be avoided in favor of class parameters as
-#  of Puppet 2.6.)
-#
-# Examples
-# --------
-#
-# @example
-#    class { 'kong':
-#      servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#    }
-#
-# Authors
-# -------
-#
-# Author Name <author@domain.com>
-#
-# Copyright
-# ---------
-#
-# Copyright 2015 Your name here, unless otherwise noted.
-#
-class kong {
-  class { 'kong::install': }
+class kong(
+	$kong_version = 'newest',
+	$ensure_config = 'file',
+	$ensure_package = 'present',
+	$kong_download_url = 'http://downloadkong.org/el6.noarch.rpm',
+	$config_url = '/etc/kong/kong.yml',
+	$kong_install_path = '/usr/local/bin/',
+	$kong_start_command = 'kong restart',
+	$nginx_working_dir = '/usr/local/kong/',
+	$proxy_port = 8000,
+	$proxy_ssl_port = 8443,
+	$admin_api_port = 8001,
+	$dnsmasq_port = 8053,
+
+){
+	validate_string($cassandra_nodes)
+	if $kong_version != "newest" {
+		$kong_version_string = "?version=$::kong_version"
+	} else{
+		$kong_version_string = ""
+	}
+	$kong_source_url = "$kong_download_url$kong_version_string"
+	
+	if ($::operatingsystem == 'CentOS'){
+
+		package{'epel-release':
+			ensure => present,
+		}
+
+		package{['dnsmasq','nc','openssl098e']:
+			ensure => present,
+			before => Package["kong"],
+			require => Package["epel-release"],
+		}
+
+		package{ 'kong':
+			provider => 'rpm',
+			ensure => $ensure_package,
+			source => $kong_source_url,
+			require => File['kong_config'],
+		}
+
+		file { 'kong_directory':
+			ensure => directory,
+			path   => '/etc/kong',
+			before => File['kong_config'],
+		}
+
+		file { 'kong_config':
+			ensure  => file,
+			content => template('kong/kong.yaml.erb'),
+			path 	=> $config_url,
+			notify  => Exec['reload_kong'],
+		}
+
+		exec { 'start_kong':
+		    command => $kong_start_command,
+		    path    => $kong_install_path,
+		    require => Package['kong'],
+		}
+
+		exec {'reload_kong':
+			command => 'kong reload',
+			path    => $kong_install_path,
+			refreshonly => true,
+		}
+	} else{
+		fail('Only CentOS 6 is currently supported')
+	}
 }
